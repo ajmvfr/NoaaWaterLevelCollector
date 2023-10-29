1dde8ac76d459d14d35fcce6920e075952de657c
ajmvfr <ajmvfr@gmail.com>
2023-10-29 14:51:54 -0400

    move main

---
+++ main.py
@@ -0,0 +1,21 @@
+from app import models
+from app.database import engine
+from app.config import settings
+from app.WaterLevelRssParse import WaterLevelRssParse, CollectWaterData
+import time
+
+models.Base.metadata.create_all(bind=engine)
+
+CollectWaterData()
+# WaterLevelRssParse('BEAP1')
+# WaterLevelRssParse('PNEP1')
+# WaterLevelRssParse('prvp1')
+# WaterLevelRssParse('mgyp1')
+# WaterLevelRssParse('dshp1')
+# WaterLevelRssParse('pttp1')
+
+
+# BEAP1
+# MGYP1
+# DSHP1
+# PTTP1
